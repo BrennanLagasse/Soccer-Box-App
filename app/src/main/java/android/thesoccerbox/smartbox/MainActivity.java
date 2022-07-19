270173c4d922da27656a4e4b70597371b87477fa
Brennan Lagasse <51427169+BrennanLagasse@users.noreply.github.com>
2022-07-19 17:02:39 -0400

    Setup box selection UI, working on utility

--- app/src/main/java/android/thesoccerbox/smartbox/MainActivity.java
+++ app/src/main/java/android/thesoccerbox/smartbox/MainActivity.java
@@ -3,12 +3,46 @@ package android.thesoccerbox.smartbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mRoomOneButton;
+    private Button mRoomTwoButton;
+    private Button mRoomThreeButton;
+    private Button mRoomFourButton;
+    private Button mNextButton;
+
+    private Button[] roomButtons = {mRoomOneButton, mRoomTwoButton, mRoomThreeButton, mRoomFourButton};
+    private boolean[] roomButtonValues = new boolean[4];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRoomOneButton = (Button) findViewById(R.id.room_one_button);
+        mRoomTwoButton = (Button) findViewById(R.id.room_two_button);
+        mRoomThreeButton = (Button) findViewById(R.id.room_three_button);
+        mRoomFourButton = (Button) findViewById(R.id.room_four_button);
+        mNextButton = (Button) findViewById(R.id.next_button);
+
+        mRoomOneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.test, Toast.LENGTH_SHORT).show();
+
+                if(roomButtonValues[0]) {
+
+                }
+                else {
+
+                }
+            }
+        });
+
+
     }
 }
