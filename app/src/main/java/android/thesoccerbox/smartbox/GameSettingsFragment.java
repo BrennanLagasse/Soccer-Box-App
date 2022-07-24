e87b26db68151f70081fdfb93244f1643e661304
Brennan Lagasse <51427169+BrennanLagasse@users.noreply.github.com>
2022-07-23 21:29:35 -0400

    Working settings page

--- app/src/main/java/android/thesoccerbox/smartbox/GameSettingsFragment.java
+++ app/src/main/java/android/thesoccerbox/smartbox/GameSettingsFragment.java
@@ -2,16 +2,21 @@ package android.thesoccerbox.smartbox;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import java.util.UUID;
+
+import static android.content.ContentValues.TAG;
 
 public class GameSettingsFragment extends Fragment {
 
@@ -20,6 +25,26 @@ public class GameSettingsFragment extends Fragment {
     private TextView mTitleView;
     private Button mSynchButton;
     private Button mStartButton;
+
+    private View mGameNumberView;
+    private EditText mValue1;
+    private Button mMinus1;
+    private Button mPlus1;
+
+    private View mGameTimeView;
+    private EditText mValue2;
+    private Button mMinus2;
+    private Button mPlus2;
+
+    private View mGameDelayView;
+    private EditText mValue3;
+    private Button mMinus3;
+    private Button mPlus3;
+
+    private View mTargetTimeView;
+    private EditText mValue4;
+    private Button mMinus4;
+    private Button mPlus4;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,6 +62,141 @@ public class GameSettingsFragment extends Fragment {
 
         mTitleView = view.findViewById(R.id.settings_title_text);
         mTitleView.setText((mGame.getTitle() + " Settings"));
+
+        // Game Number
+        {
+            mGameNumberView = view.findViewById(R.id.num_games_selection);
+
+            TextView mTitle = mGameNumberView.findViewById(R.id.value_label_text);
+            mTitle.setText(getString(R.string.game_number_prompt));
+
+            mValue1 = mGameNumberView.findViewById(R.id.value_text);
+
+            mMinus1 = mGameNumberView.findViewById(R.id.minus_button);
+            mMinus1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int val = Integer.parseInt(mValue1.getText().toString());
+
+                    if (val > 1) {
+                        String newVal = Integer.toString(val - 1);
+                        mValue1.setText(newVal);
+                    }
+                }
+            });
+
+            mPlus1 = mGameNumberView.findViewById(R.id.plus_button);
+            mPlus1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int val = Integer.parseInt(mValue1.getText().toString());
+                    String newVal = Integer.toString(val + 1);
+                    mValue1.setText(newVal);
+                }
+            });
+        }
+
+        //Game Time
+        {
+            mGameTimeView = view.findViewById(R.id.game_time_selection);
+
+            TextView mTitle = mGameTimeView.findViewById(R.id.value_label_text);
+            mTitle.setText(getString(R.string.game_time_prompt));
+
+            mValue2 = mGameTimeView.findViewById(R.id.value_text);
+            mValue2.setText(getString(R.string.default_game_length));
+
+            mMinus2 = mGameTimeView.findViewById(R.id.minus_button);
+            mMinus2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int val = Integer.parseInt(mValue2.getText().toString());
+
+                    if (val > 1) {
+                        String newVal = Integer.toString(val - 5);
+                        mValue2.setText(newVal);
+                    }
+                }
+            });
+
+            mPlus2 = mGameTimeView.findViewById(R.id.plus_button);
+            mPlus2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int val = Integer.parseInt(mValue2.getText().toString());
+                    String newVal = Integer.toString(val + 5);
+                    mValue2.setText(newVal);
+                }
+            });
+        }
+
+        //After Game Delay
+        {
+            mGameDelayView = view.findViewById(R.id.game_delay_selection);
+
+            TextView mTitle = mGameDelayView.findViewById(R.id.value_label_text);
+            mTitle.setText(getString(R.string.game_delay_prompt));
+
+            mValue3 = mGameDelayView.findViewById(R.id.value_text);
+            mValue3.setText(getString(R.string.default_delay_length));
+
+            mMinus3 = mGameDelayView.findViewById(R.id.minus_button);
+            mMinus3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int val = Integer.parseInt(mValue3.getText().toString());
+
+                    if (val > 1) {
+                        String newVal = Integer.toString(val - 10);
+                        mValue3.setText(newVal);
+                    }
+                }
+            });
+
+            mPlus3 = mGameDelayView.findViewById(R.id.plus_button);
+            mPlus3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int val = Integer.parseInt(mValue3.getText().toString());
+                    String newVal = Integer.toString(val + 10);
+                    mValue3.setText(newVal);
+                }
+            });
+        }
+
+        //Target Time
+        {
+            mTargetTimeView = view.findViewById(R.id.target_time_selection);
+
+            TextView mTitle = mTargetTimeView.findViewById(R.id.value_label_text);
+            mTitle.setText(getString(R.string.target_time_prompt));
+
+            mValue4 = mTargetTimeView.findViewById(R.id.value_text);
+            mValue4.setText(getString(R.string.default_target_time));
+
+            mMinus4 = mTargetTimeView.findViewById(R.id.minus_button);
+            mMinus4.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int val = Integer.parseInt(mValue4.getText().toString());
+
+                    if (val > 1) {
+                        String newVal = Integer.toString(val - 1);
+                        mValue4.setText(newVal);
+                    }
+                }
+            });
+
+            mPlus4 = mTargetTimeView.findViewById(R.id.plus_button);
+            mPlus4.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int val = Integer.parseInt(mValue4.getText().toString());
+                    String newVal = Integer.toString(val + 1);
+                    mValue4.setText(newVal);
+                }
+            });
+        }
 
         mSynchButton = view.findViewById(R.id.synchronous_button);
 
