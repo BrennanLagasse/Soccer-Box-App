4ae8bb0acb43b95542a260ef2ec64f7343aeb72f
Brennan Lagasse <brennan.lagasse@gmail.com>
2023-01-03 16:44:46 -0500

    Updated readability, renamed files, and added code paths for single player games

--- app/src/main/java/android/thesoccerbox/smartbox/GameManager.java
+++ app/src/main/java/android/thesoccerbox/smartbox/GameManager.java
@@ -25,17 +25,39 @@ public class GameManager {
     }
 
     private GameManager(Context context) {
-        mOnePlayerGames.add(new Game("Standard", "One target", 1,  false, false, "1p_standard.py"));
-        mOnePlayerGames.add(new Game("Standard Next", "One target with next target indicated", 1, false, false, "1p_standard_next.py"));
-        mOnePlayerGames.add(new Game("Standard Color", "All targets lit, hit the correct color", 1, false, false, "???"));
-        mOnePlayerGames.add(new Game("Double Tap Standard", "One target that must be hit twice",1, false, false, "???"));
-        mOnePlayerGames.add(new Game("Two Targets Standard", "Two targets, either must be hit", 1, false, false, "???"));
-        mOnePlayerGames.add(new Game("Both Targets Standard", "Two targets, both must be hit", 1, false, false, "???"));
-        mOnePlayerGames.add(new Game("Rondo Scan", "One target, front only", 1, false, false, "???"));
-        mOnePlayerGames.add(new Game("Shoulder Check 1", "Hit front target matching back target", 1, false, false, "???"));
-        mOnePlayerGames.add(new Game("Shoulder Check 2", "Hit front target matching back target", 1, false, false, "???"));
-        mOnePlayerGames.add(new Game("Color Find", "Hit target color announced by speaker", 1, false, false, "???"));
+        // One Player Games
+        mOnePlayerGames.add(new Game("Standard", "One target",
+                1,  false, false,
+                "1p_standard.py"));
+        mOnePlayerGames.add(new Game("Standard Next", "One target with next target indicated",
+                1, false, false,
+                "1p_standard_next.py"));
+        mOnePlayerGames.add(new Game("Standard Color", "All targets lit, hit the correct color",
+                1, false, false,
+                "1p_standard_color.py"));
+        mOnePlayerGames.add(new Game("Double Tap Standard", "One target that must be hit twice",
+                1, false, false,
+                "1p_double_standard.py"));
+        mOnePlayerGames.add(new Game("Two Targets Standard", "Two targets, either must be hit",
+                1, false, false,
+                "1p_two_targets.py"));
+        mOnePlayerGames.add(new Game("Both Targets Standard", "Two targets, both must be hit",
+                1, false, false,
+                "1p_both_targets.py"));
+        mOnePlayerGames.add(new Game("Rondo Scan", "One target, front only",
+                1, false, false,
+                "1p_rondo.py"));
+        mOnePlayerGames.add(new Game("Shoulder Check Basic", "Hit front target matching back target",
+                1, false, false,
+                "1p_shoulder_check_basic.py"));
+        mOnePlayerGames.add(new Game("Shoulder Check Advanced", "Hit front target matching back target",
+                1, false, false,
+                "1p_shoulder_check_advanced.py"));
+        mOnePlayerGames.add(new Game("Color Find", "Hit target color announced by speaker",
+                1, false, false,
+                "1p_color_find.py"));
 
+        // Two Player Games
         mTwoPlayerGames.add(new Game("Standard Simultaneous", "One target for each player, targets are unlinked", 2, false, true, "2p_standard_sync"));
         mTwoPlayerGames.add(new Game("Standard Competitive", "One target for each player, targets are reset after either target is hit", 2, true, false, "2p_standard_async"));
         mTwoPlayerGames.add(new Game("Standard Next", "One target for each player, next targets are indicated", 2, true, true, "???"));
@@ -47,6 +69,7 @@ public class GameManager {
         mTwoPlayerGames.add(new Game("Three Goal Game Timed", "Each player must hit the opposing three targets", 2, false, false, "???"));
         mTwoPlayerGames.add(new Game("1v1", "One target for each player that changes after a set interval", 2, true, false, "???"));
 
+        // Tests
         mTestGames.add(new Game("LED Test", "Lights up all LEDs one target at a time", 0, false, false, "light_test.py"));
         mTestGames.add(new Game("Scoreboard Test", "Displays score on screen", 0, false, false,  "???"));
         mTestGames.add(new Game("Speaker Test", "Cycles through all game sounds", 0, false, false,  "1p_standard.py 1 0 2 60"));
